1c2b71d123487f3eef28f9f07651880676d03630
Aissam <37519518+Aissam-gif@users.noreply.github.com>
2021-01-09 13:11:20 +0100

    Update Jeu.java

--- Jeu.java
+++ Jeu.java
@@ -9,45 +9,25 @@ public class Jeu {
 	private String[] Words = {"pomme","framboise","fraise","melon","orange","pistache","datte",
 			"tomate","avocat","pistache","amande","noix","kaki",
 			"lime","banane","clementine","kiwi","mangue","abricot","pasteque"};
-	private String WordGenerated; //Stockage mot générer de la liste
-	private char[] Trait;
+	private String WordGenerated; // Stockage mot générer de la liste
+	private char[] Trait; 		  // Modifier l'etat du trait 
 	private int hintnbr = 1;
 	private int indexWordGenerated;
 	public Jeu() {
-		/*
-		 * A la création du jeu on doit génerer Un mot au hasard
-		 */
+		/*	 
+		 *   A la création du jeu on génere Un mot au hasard
+		 */  
 		this.indexWordGenerated = (int) (Math.random()*(Words.length));
 		this.WordGenerated = Words[indexWordGenerated];
 		this.MaxEssai = 5;
 		this.nbrEssaiErrone = 0;
 	}
 	
-	public int getnbrEssaiErrone() {
-		return nbrEssaiErrone;
-	}
-	public void setnbrEssaiErrone(int nbrEssaiErrone) {
-		this.nbrEssaiErrone = nbrEssaiErrone;
-	}
-	public int getMaxEssai() {
-		return MaxEssai;
-	}
-	// Index du mot pris au hasard
-	public int IndexMotHasard() {
-		int index = (int) (Math.random()*(Words.length));
-		this.setWordGenerated(Words[index]);
-		return index; 
-		
-	}
-	public String getWordGenerated() {
-		return WordGenerated;
-	}
-
-	public void setWordGenerated(String wordGenerated) {
-		WordGenerated = wordGenerated;
-	}
-
-	// Change Word from lower to upper case
+	
+	/*
+	 * Fonction qui permet de transferer un tableau des caractères minuscule
+	 * 		en Majuscule
+	 */
 	public char[] LowerToUpper(char[] LowerWord) {
 		char[] UpperCase = new char[LowerWord.length];
 		for (int i=0;i<LowerWord.length;i++) {
@@ -55,14 +35,13 @@ public class Jeu {
 		}
 		return UpperCase;
 	}
+	
 	/*
 	 * Génerer les caractères du mot brouiller
+	 *  Pomme -> A D E O P Q M M 
 	 */
 	public char[] GenerateChar() {
-		String Mot  = Words[indexWordGenerated];
-		/*
-		String Mot  = Words[IndexMotHasard()];
-		setWordGenerated(Mot); */
+		String Mot  = this.WordGenerated;
 		char[] FinalString = new char[Mot.length() + 5];
 		char[] MotChar = Mot.toCharArray();
 	
@@ -80,10 +59,15 @@ public class Jeu {
 		FinalString = LowerToUpper(FinalString);
 		return FinalString;
 	}
-		/*
-		 *  Affichage Des Traits
-		 */
 	
+	
+	/*
+	 * Modifier les Trait avec le nouveau caractere entré
+	 * 	- Si le caractère appartient au mot On le place dans son propre indice , 
+	 * 		On retourne true
+	 * 	- Sinon on incremente le nombre d'essai errone entré
+	 * 		On retourne false
+	 */
 	public boolean Trait(char Input) {
 		if (CharAppartientMot(Input, WordGenerated, Trait)) 
 		{
@@ -95,22 +79,16 @@ public class Jeu {
 			}
 			return true;
 		}
-		else {
-			//On prend un mot aleatoire et on le place
-			nbrEssaiErrone++;
-			/*
-			for (int i=0;i<Trait.length;i++) {
-				if (Trait[i] == '_') {
-					this.Trait[i] = Character.toUpperCase(WordGenerated.charAt(i));
-					break;
-				}
-			} */
+		else {			
+			nbrEssaiErrone++;		
 		}
 		return false;
 	}
+	
+	/*
+	 * 	Lors de l'appel d'un hint on place un caractere automatique pour l'utilisateur
+	 */
 	public void UseHint() {
-		//nbrEssaiErrone++;
-		
 		for (int i=0;i<Trait.length;i++) {
 			if (Trait[i] == '_') {
 				this.Trait[i] = Character.toUpperCase(WordGenerated.charAt(i));
@@ -120,6 +98,71 @@ public class Jeu {
 		hintnbr++;
 	}
 	
+	/*
+	 * 	Calcul de nombre de trait restant vide
+	 */
+	public int Traitleftnbr() {
+		int compteur = 0;
+		for (int i=0;i<Trait.length;i++) {
+			if (Trait[i] == '_') {
+				compteur++;
+			}
+		}
+		return compteur;
+	}
+	
+	/*
+	 * 	Dans le debut de jeu on doit initialiser le tableau des traits par des tirés
+	 */
+	public char[] InitTrait() {
+		char[] trait = new char[WordGenerated.length()];
+		for (int i=0;i<trait.length;i++) trait[i] = '_';
+		return trait;
+	}
+	
+	
+	/*
+	 * 	Verficiation Si le caractere entré (Input) se trouve bien dans le mot et
+	 * 		il n'est pas déja present dans le tableau des traits
+	 */
+	public boolean CharAppartientMot(char Input,String WordGenerated,char[] trait) {
+		char[] Word = WordGenerated.toCharArray();
+		for (int i=0;i<Word.length;i++) {
+			if ( (Word[i] == Input || Word[i] == Character.toLowerCase(Input)) && trait[i] == '_') {
+				return true;
+		}
+	}
+		return false;
+}
+	/*
+	 * 		Pour avoir index d'un caractere entrée qui vérifie les conditions 
+	 * 		sité au dessus
+	 */
+	public int getIndexChar(char Input,String WordGenerated,char[] trait) {
+		char[] Word = WordGenerated.toCharArray();
+		for (int i=0;i<Word.length;i++) {
+			if ((Word[i] == Input || Word[i] == Character.toLowerCase(Input)) && trait[i] == '_') return i;
+		}
+		return -1;
+	}
+	/*
+	 * 			GETTERS AND SETTERS		
+	 */
+	public int getnbrEssaiErrone() {
+		return nbrEssaiErrone;
+	}
+	public void setnbrEssaiErrone(int nbrEssaiErrone) {
+		this.nbrEssaiErrone = nbrEssaiErrone;
+	}
+	public int getMaxEssai() {
+		return MaxEssai;
+	}
+	public String getWordGenerated() {
+		return WordGenerated;
+	}
+	public void setWordGenerated(String wordGenerated) {
+		WordGenerated = wordGenerated;
+	}
 	public int getHintnbr() {
 		return hintnbr;
 	}
@@ -130,37 +173,5 @@ public class Jeu {
 
 	public void setTrait(char[] trait) {
 		Trait = trait;
-	}
-	public int Traitleftnbr() {
-		int compteur = 0;
-		for (int i=0;i<Trait.length;i++) {
-			if (Trait[i] == '_') {
-				compteur++;
-			}
-		}
-		return compteur;
-	}
-	//Initialiser Trait
-	public char[] InitTrait() {
-		char[] trait = new char[WordGenerated.length()];
-		for (int i=0;i<trait.length;i++) trait[i] = '_';
-		return trait;
-	}
-	//Test Si le Caractère se trouve dans le Mot
-	public boolean CharAppartientMot(char Input,String WordGenerated,char[] trait) {
-		char[] Word = WordGenerated.toCharArray();
-		for (int i=0;i<WordGenerated.length();i++) {
-			if ( (Word[i] == Input || Word[i] == Character.toLowerCase(Input)) && trait[i] == '_') {
-				return true;
-		}
-	}
-		return false;
-}
-	public int getIndexChar(char Input,String WordGenerated,char[] trait) {
-		char[] Word = WordGenerated.toCharArray();
-		for (int i=0;i<Word.length;i++) {
-			if ((Word[i] == Input || Word[i] == Character.toLowerCase(Input)) && trait[i] == '_') return i;
-		}
-		return -1;
 	}
 }
