4990d2a65fc3fa13de1c1436c6d95d13539da580
Aissam <37519518+Aissam-gif@users.noreply.github.com>
2021-01-15 23:14:31 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@
 public class Main {
 
 	public static void main(String[] args) {
-			
+		
 		Jeu Game = new Jeu();
 		Deroulement Der = new Deroulement(Game);
-		Menu.menu(Der,Game);
-		//Der.start();
-	
+		Menu.menuTheme(Der,Game);
+		
+		
 	}
 
 }
