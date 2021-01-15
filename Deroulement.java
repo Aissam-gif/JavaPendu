441d911ddca57c3803a86d18c136564c1e4a0d36
Aissam <37519518+Aissam-gif@users.noreply.github.com>
2021-01-15 17:18:51 +0100

    Update Deroulement.java

--- Deroulement.java
+++ Deroulement.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 /*
- *   Classe Déroulement permet d'assurer le déroulement du programme ,
- *   qui arrange toutes les méthodes créer en classe Jeu et vérifie des conditions
+ *   Classe Dï¿½roulement permet d'assurer le dï¿½roulement du programme ,
+ *   qui arrange toutes les mï¿½thodes crï¿½er en classe Jeu et vï¿½rifie des conditions
  *   d'arret de jeu  
  */
 public class Deroulement {
 	/*
-	 * Pour chaque déroulement On dispose d'un nouveau mot ,donc propriétée Game
-	 * permet d'associer a chaque deroulement des proprietés et des methode de JEU
-	 * depend de ce nom génerer
+	 * Pour chaque dï¿½roulement On dispose d'un nouveau mot ,donc propriï¿½tï¿½e Game
+	 * permet d'associer a chaque deroulement des proprietï¿½s et des methode de JEU
+	 * depend de ce nom gï¿½nerer
 	 */
 	private Jeu Game;
-	// InputConsole pour lire l'entrer de l'utilisateur (le caractere entré)
+	// InputConsole pour lire l'entrer de l'utilisateur (le caractere entrï¿½)
 	private char InputConsole;
 	// hint pour lire l'entrer de l'utilisateur quand il demande un hint
 	public String hint;
-	// Garder les Caracteres génerer automatique ( B C E D S A )  dans une propriétée 
+	// Garder les Caracteres gï¿½nerer automatique ( B C E D S A )  dans une propriï¿½tï¿½e 
 	private char[] CaractereGenerer;
 	public static final Scanner Console = new Scanner(System.in);
 	/*
@@ -31,23 +31,23 @@ public class Deroulement {
 	
 	/*
 	 *  Methode Start consiste a faire derouler le jeu , dans chaque tour 
-	 *   1 - On affiche l'état des traits ( _ _ _ _ )
-	 *   2 - ..  ...    les caractères de l'aide ( B C E D S A )
-	 *   3 - On demande utilisateur d'entrer un caractère :
-	 *   4 - On fait les vérification des données entrés
+	 *   1 - On affiche l'ï¿½tat des traits ( _ _ _ _ )
+	 *   2 - ..  ...    les caractï¿½res de l'aide ( B C E D S A )
+	 *   3 - On demande utilisateur d'entrer un caractï¿½re :
+	 *   4 - On fait les vï¿½rification des donnï¿½es entrï¿½s
 	 *   5 - On affiche nombre d'essais restans
 	 */
 	public void start() {
 		 System.out.println("Bienvenu Dans Votre Jeu !");
 		 System.out.println("Vous Avez " + Game.getMaxEssai() + " Essais ");
 		/*
-		 *  SetTrait est Un accesseur de la proprieté du trait dans la classe Jeu
-		 *  InitTrait Pour initialiser le nombre de Trait selon le mot définie au hasard
+		 *  SetTrait est Un accesseur de la proprietï¿½ du trait dans la classe Jeu
+		 *  InitTrait Pour initialiser le nombre de Trait selon le mot dï¿½finie au hasard
 		 */
 		 
 		Game.setTrait(Game.InitTrait());
 		do {
-			//Chaque tour affichage état traits
+			//Chaque tour affichage ï¿½tat traits
 			DisplayTrait();
 			// Affichage caractere
 			DisplayCaracter();
@@ -65,28 +65,28 @@ public class Deroulement {
 				System.out.println();
 			}
 			/*
-			 * Si le caractère ou la chaine(premier caractère) entré n'est pas "hint" et il'est juste
+			 * Si le caractï¿½re ou la chaine(premier caractï¿½re) entrï¿½ n'est pas "hint" et il'est juste
 			 */
 			if (Game.Trait(InputConsole) && !hint.equalsIgnoreCase("hint")) {
 				System.out.println("-->> Vous avez bien tourver le caractere ! <<--");
 				System.out.println();
 			}
 			/*
-			 * Si le caractère ou la chaine entré n'est pas "hint"
+			 * Si le caractï¿½re ou la chaine entrï¿½ n'est pas "hint"
 			 */
 			else if (!hint.equalsIgnoreCase("hint")){
 				System.out.println("-->> Oops , Vous avez mal choisie le caractere ! <<--");
 				System.out.println();
-				Dessin.draw();
+				Dessin.draw();  
 			} 
 			/*
-			 * Si la chaine entrée etait un "hint"
+			 * Si la chaine entrï¿½e etait un "hint"
 			 */
 			else {
 				/*
 				 * Les cas ou l'utilisateur peux  utiliser un hint :
-				 * 	1 - Si le Nombre des hint utilisé ne depasse pas 2
-				 * 	2 - Si il reste plus d'un trait de caractère vide dans le mot ( A B R _ I C _ T)
+				 * 	1 - Si le Nombre des hint utilisï¿½ ne depasse pas 2
+				 * 	2 - Si il reste plus d'un trait de caractï¿½re vide dans le mot ( A B R _ I C _ T)
 				 *  3 - Si le nombre d'essais restant n'est pas a zero
 				 */
 				if (Game.getHintnbr() <= 2 && Game.Traitleftnbr() > 1 && (Game.getMaxEssai()-Game.getnbrEssaiErrone())>=1 )  {
@@ -111,7 +111,12 @@ public class Deroulement {
 		 * Si le nombre d'essai errone n'a pas atteint le max 
 		 */
 		if ( Game.getMaxEssai() - Game.getnbrEssaiErrone() > 0) {
-			System.out.println("Bravo , Vous Avez Gagné le Mot a ete bien : " + Game.getWordGenerated());
+			System.out.println("|--------------------------------|");
+			System.out.println("|--------------------------------|");
+			System.out.println("|----------- BRAVO !!! ----------|");
+			System.out.println("|--------------------------------|");
+			System.out.println("|--------------------------------|");
+			System.out.println("Vous Avez Gagnï¿½ le Mot a ete bien : " + Game.getWordGenerated());
 			for (int i=0;i<5 ;i++) {
 				System.out.println();
 			}
@@ -159,7 +164,7 @@ public class Deroulement {
 	}
 	/*
 	 *  A chaque fois on termine un deroulement on doit faire une reintialisation
-	 *  du jeu , en créant une instance je noubeau jeu et de generer un nouveau mot
+	 *  du jeu , en crï¿½ant une instance je noubeau jeu et de generer un nouveau mot
 	 *  , reintialiser le compteur du dessin pendu
 	 */
 	public Jeu ResetGame() {
